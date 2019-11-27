4a2a8c090ed6b4fd1df8c41fdef9f707b62a5eb5
Nikita <mizev-nr@alamics.ru>
2019-11-27 17:45:21 +0500

    Rebuild

--- index.js
+++ index.js
@@ -4,78 +4,56 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var sscs = {
+  toFixed: function toFixed(fix) {
+    this.__value = parseFloat(this.__value.toFixed(fix));
+    return this;
+  },
+  toFirstNumberFixed: function toFirstNumberFixed() {
+    var fill = function fill(n) {
+      var zeros = '';
+      for (var i = 0; i < n; i++) {
+        zeros += '0';
+      }
+      return zeros;
+    };
 
-var SSCS = function () {
-  function SSCS(value, config) {
-    _classCallCheck(this, SSCS);
+    var str = this.__value.toString().split('.')[1] || this.__value.toString().split(',')[1];
 
-    this.__value = value;
-    this.__error = config && config.error || '-';
+    if (!str) return this;
 
-    if (!value || isNaN(value)) {
-      return;
-    }
+    var firstNumIndex = [].concat(_toConsumableArray(str)).findIndex(function (x) {
+      return parseInt(x) > 0;
+    });
+    var rounded = Math.round([[].concat(_toConsumableArray(str)).slice(firstNumIndex)[0], '.'].concat(_toConsumableArray([].concat(_toConsumableArray(str)).slice(firstNumIndex + 1))).join(''));
+    this.__value = '0.' + fill(firstNumIndex) + rounded;
+
+    return this;
+  },
+  round: function round() {
+    this.__value = Math.round(this.__value);
+    return this;
+  },
+  separate: function separate() {
+    this.__value = this.__value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    return this;
+  },
+  replaceDots: function replaceDots() {
+    this.__value = this.__value.toString().replace('.', ',');
+    return this;
+  },
+  valueOf: function valueOf() {
+    return this.__value;
   }
-
-  _createClass(SSCS, [{
-    key: 'toFixed',
-    value: function toFixed(fix) {
-      this.__value = parseFloat(this.__value.toFixed(fix));
-      return this;
-    }
-  }, {
-    key: 'toFirstNumberFixed',
-    value: function toFirstNumberFixed() {
-      var fill = function fill(n) {
-        var zeros = '';
-        for (var i = 0; i < n; i++) {
-          zeros += '0';
-        }
-        return zeros;
-      };
-
-      var str = this.__value.toString().split('.')[1] || this.__value.toString().split(',')[1];
-      var firstNumIndex = [].concat(_toConsumableArray(str)).findIndex(function (x) {
-        return parseInt(x) > 0;
-      });
-      var rounded = Math.round([[].concat(_toConsumableArray(str)).slice(firstNumIndex)[0], '.'].concat(_toConsumableArray([].concat(_toConsumableArray(str)).slice(firstNumIndex + 1))).join(''));
-      this.__value = '0.' + fill(firstNumIndex) + rounded;
-
-      return this;
-    }
-  }, {
-    key: 'round',
-    value: function round() {
-      this.__value = Math.round(this.__value);
-      return this;
-    }
-  }, {
-    key: 'separate',
-    value: function separate() {
-      this.__value = this.__value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-      return this;
-    }
-  }, {
-    key: 'replaceDots',
-    value: function replaceDots() {
-      this.__value = this.__value.toString().replace('.', ',');
-      return this;
-    }
-  }, {
-    key: 'valueOf',
-    value: function valueOf() {
-      return this.__value;
-    }
-  }]);
-
-  return SSCS;
-}();
+};
 
 exports.default = function (value, config) {
-  return new SSCS(value, config);
+  return _extends({
+    __value: value && !isNaN(value) ? value : config.error || '0',
+    __config: config
+  }, sscs);
 };
