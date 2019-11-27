8d524dd1543465b03bb8424e0a8ce29338a356c1
Nikita <mizev-nr@alamics.ru>
2019-11-27 17:08:53 +0500

    add toFirstNumberFixed()

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-exports.default = function (value, config) {
-  return new SSCS(value, config);
-};
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -20,7 +18,7 @@ var SSCS = function () {
     this.__error = config && config.error || '-';
 
     if (!value || isNaN(value)) {
-      return this.__error;
+      return;
     }
   }
 
@@ -31,9 +29,41 @@ var SSCS = function () {
       return this;
     }
   }, {
+    key: 'toFirstNumberFixed',
+    value: function toFirstNumberFixed() {
+      var fill = function fill(n) {
+        var zeros = '';
+        for (var i = 0; i < n; i++) {
+          zeros += '0';
+        }
+        return zeros;
+      };
+
+      var str = this.__value.toString().split('.')[1] || this.__value.toString().split(',')[1];
+      var firstNumIndex = [].concat(_toConsumableArray(str)).findIndex(function (x) {
+        return parseInt(x) > 0;
+      });
+      var rounded = Math.round([[].concat(_toConsumableArray(str)).slice(firstNumIndex)[0], '.'].concat(_toConsumableArray([].concat(_toConsumableArray(str)).slice(firstNumIndex + 1))).join(''));
+      this.__value = '0.' + fill(firstNumIndex) + rounded;
+
+      return this;
+    }
+  }, {
     key: 'round',
     value: function round() {
-      __value = Math.round(__value);
+      this.__value = Math.round(this.__value);
+      return this;
+    }
+  }, {
+    key: 'separate',
+    value: function separate() {
+      this.__value = this.__value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+      return this;
+    }
+  }, {
+    key: 'replaceDots',
+    value: function replaceDots() {
+      this.__value = this.__value.toString().replace('.', ',');
       return this;
     }
   }, {
@@ -46,4 +76,6 @@ var SSCS = function () {
   return SSCS;
 }();
 
-;
+exports.default = function (value, config) {
+  return new SSCS(value, config);
+};
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ class SSCS {
     this.__error = config && config.error || '-'
     
     if (!value || isNaN(value)) {
-      return this.__error;
+      return;
     }
   }
 
@@ -13,8 +13,33 @@ class SSCS {
     return this;
   }
 
+  toFirstNumberFixed() {    
+    const fill = (n) => {
+      let zeros = '';
+      for(let i = 0; i < n; i++) { zeros += '0' }
+      return zeros;
+    }
+
+    const str = this.__value.toString().split('.')[1] || this.__value.toString().split(',')[1];
+    const firstNumIndex = [...str].findIndex(x => parseInt(x) > 0);
+    const rounded = (Math.round([[...str].slice(firstNumIndex)[0], '.', ...[...str].slice(firstNumIndex + 1)].join('')));
+    this.__value = `0.${fill(firstNumIndex)}${rounded}`;
+    
+    return this;
+  }
+
   round() {
     this.__value = Math.round(this.__value);
+    return this;
+  }
+
+  separate() {
+    this.__value = this.__value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    return this;
+  }
+
+  replaceDots() {
+    this.__value = this.__value.toString().replace('.', ',');
     return this;
   }
 
@@ -23,6 +48,4 @@ class SSCS {
   }
 }
 
-export default function(value, config) {
-  return new SSCS(value, config);
-};
+export default (value, config) => new SSCS(value, config);
