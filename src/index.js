06f56594fb903fb29f263750f491844da252f417
Nikita <mizev-nr@alamics.ru>
2019-11-27 17:44:54 +0500

    Rewrite from class to object

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-class SSCS {
-  constructor(value, config) {
-    this.__value = value;
-    this.__error = config && config.error || '-'
-    
-    if (!value || isNaN(value)) {
-      return;
-    }
-  }
-
+const sscs = {
   toFixed(fix) {
     this.__value = parseFloat(this.__value.toFixed(fix));
     return this;
-  }
+  },
 
   toFirstNumberFixed() {    
     const fill = (n) => {
@@ -21,31 +12,40 @@ class SSCS {
     }
 
     const str = this.__value.toString().split('.')[1] || this.__value.toString().split(',')[1];
+    
+    if (!str) return this;
+
     const firstNumIndex = [...str].findIndex(x => parseInt(x) > 0);
     const rounded = (Math.round([[...str].slice(firstNumIndex)[0], '.', ...[...str].slice(firstNumIndex + 1)].join('')));
     this.__value = `0.${fill(firstNumIndex)}${rounded}`;
     
     return this;
-  }
+  },
 
   round() {
     this.__value = Math.round(this.__value);
     return this;
-  }
+  },
 
   separate() {
     this.__value = this.__value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
     return this;
-  }
+  },
 
   replaceDots() {
     this.__value = this.__value.toString().replace('.', ',');
     return this;
-  }
+  },
 
   valueOf() { 
     return this.__value;
-  }
+  },
 }
 
-export default (value, config) => new SSCS(value, config);
+export default (value, config) => {
+  return {
+    __value: value && !isNaN(value) ? value : config.error || '0',
+    __config: config,
+    ...sscs
+  }
+};
